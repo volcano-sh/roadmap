c41f2ee7dfd0ddc6e8a98ffbb94b8de6798abd33
merryzhou <merryzhou0882@gmail.com>
2021-07-05 14:17:55 +0800

    fix e2e
    Signed-off-by: merryzhou <merryzhou0882@gmail.com>

--- pkg/controllers/cache/cache.go
+++ pkg/controllers/cache/cache.go
@@ -23,7 +23,7 @@ import (
 
 	"golang.org/x/time/rate"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog"
@@ -312,7 +312,7 @@ func (jc *jobCache) TaskFailed(jobKey, taskName string) bool {
 			break
 		}
 	}
-	if taskReplicas <= 0 {
+	if taskReplicas <= 0 || maxRetry == -1 {
 		return false
 	}
 
