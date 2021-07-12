3cceb8b745751b844adc02e2b74076ef834c550d
Thor-wl <13164644535@163.com>
2021-07-12 15:49:52 +0800

    update LessInSomeDimension function
    Signed-off-by: Thor-wl <13164644535@163.com>

--- pkg/scheduler/api/resource_info_test.go
+++ pkg/scheduler/api/resource_info_test.go
@@ -671,11 +671,34 @@ func TestMinDimensionResource(t *testing.T) {
 }
 
 func TestLessInSomeDimension(t *testing.T) {
-	tests := []struct {
+	testsForDefaultZero := []struct {
 		resource1 *Resource
 		resource2 *Resource
 		expected  bool
 	}{
+		{
+			resource1: &Resource{},
+			resource2: &Resource{},
+			expected:  false,
+		},
+		{
+			resource1: &Resource{},
+			resource2: &Resource{
+				MilliCPU:        2000,
+				Memory:          2000,
+				ScalarResources: map[v1.ResourceName]float64{"scalar.test/scalar1": 1000, "hugepages-test": 2000},
+			},
+			expected: true,
+		},
+		{
+			resource1: &Resource{
+				MilliCPU:        2000,
+				Memory:          2000,
+				ScalarResources: map[v1.ResourceName]float64{"scalar.test/scalar1": 1000, "hugepages-test": 2000},
+			},
+			resource2: &Resource{},
+			expected:  false,
+		},
 		{
 			resource1: &Resource{
 				MilliCPU:        2000,
@@ -756,8 +779,47 @@ func TestLessInSomeDimension(t *testing.T) {
 		},
 	}
 
-	for _, test := range tests {
-		flag := test.resource1.LessInSomeDimension(test.resource2)
+	testsForDefaultInfinity := []struct {
+		resource1 *Resource
+		resource2 *Resource
+		expected  bool
+	}{
+		{
+			resource1: &Resource{
+				MilliCPU:        2000,
+				Memory:          2000,
+				ScalarResources: map[v1.ResourceName]float64{"hugepages-test": 2000},
+			},
+			resource2: &Resource{
+				MilliCPU:        2000,
+				Memory:          2000,
+				ScalarResources: map[v1.ResourceName]float64{"scalar.test/scalar1": 1000, "hugepages-test": 2000},
+			},
+			expected: false,
+		},
+		{
+			resource1: &Resource{
+				MilliCPU:        2000,
+				Memory:          2000,
+				ScalarResources: map[v1.ResourceName]float64{"scalar.test/scalar1": 1000, "hugepages-test": 2000},
+			},
+			resource2: &Resource{
+				MilliCPU:        2000,
+				Memory:          2000,
+				ScalarResources: map[v1.ResourceName]float64{"hugepages-test": 2000},
+			},
+			expected: true,
+		},
+	}
+
+	for _, test := range testsForDefaultZero {
+		flag := test.resource1.LessInSomeDimension(test.resource2, Zero)
+		if !reflect.DeepEqual(test.expected, flag) {
+			t.Errorf("expected: %#v, got: %#v", test.expected, flag)
+		}
+	}
+	for _, test := range testsForDefaultInfinity {
+		flag := test.resource1.LessInSomeDimension(test.resource2, Infinity)
 		if !reflect.DeepEqual(test.expected, flag) {
 			t.Errorf("expected: %#v, got: %#v", test.expected, flag)
 		}
